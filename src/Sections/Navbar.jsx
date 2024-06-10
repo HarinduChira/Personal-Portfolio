708b32763549f73821879a0e66fa4093d2750e21
HarinduChira <basnaya-ps19152@stu.kln.ac.lk>
2024-06-11 01:38:24 +0530

    ReCreated The Portfolio with tailwind css

---
+++ src/Sections/Navbar.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { Link as ScrollLink } from 'react-scroll'
+import { chira } from '../assets/chira-dark.jpg'
+
+const Navbar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Navbar
